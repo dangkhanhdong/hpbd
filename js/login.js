fe2c45d8bc34d43ecea58e5f31257a65b258d960
Dong Dang <khanhdong1702pro@gmail.com>
2022-04-20 23:00:44 +0700

    birthday

---
+++ js/login.js
@@ -0,0 +1,18 @@
+function loginValue()
+		{
+			var password = document.getElementById("password").value;
+                var y = document.getElementById("clear");
+				var z = document.getElementById("all");			
+			if(password=="03052003")
+			{
+				alert('Oke, pass chị nhập đã chính xác!!!');
+						y.remove(y);
+						z.style.display = 'block';
+				 return false;
+        	}
+			else{
+				alert("Pass chị nhập sai rồi, pass có 8 kí tự nha!");
+				return false;
+			}
+	}
+			
