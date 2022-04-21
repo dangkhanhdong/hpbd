c7a6f2cde04f45b609979a25bd43c8a55e9aca36
DKD <80376275+dangkhanhdong@users.noreply.github.com>
2022-04-21 22:41:27 +0700

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,25 +1,21 @@
 function loginValue()
 		{
-			var n = 0;
-			while(n==0)
+	
+			var password = document.getElementById("password").value;
+			var y = document.getElementById("clear");
+			var z = document.getElementById("all");			
+			if(password=="03052003")
 			{
-				var password = document.getElementById("password").value;
-
-				 var y = document.getElementById("clear");
-					var z = document.getElementById("all");			
-				if(password=="03052003")
-				{
-					alert('Oke, pass chị nhập đã chính xác!!!');
-					y.remove(y);
-					z.style.display = 'block';
-					document.getElementById("player").play();
-					n = 1;
-				}
-				else{
-					n = 0;
-					alert("Pass chị nhập sai rồi, pass có 8 kí tự nha!");
-					location.reload();	
-				}
+			 	alert('Oke, pass chị nhập đã chính xác!!!');
+				y.remove(y);
+				z.style.display = 'block';
+				document.getElementById("player").play();
+			}
+			else{
+					 
+				alert("Pass chị nhập sai rồi, pass có 8 kí tự nha!");
+				location.reload();	
 			}
 		}
+	
 			
